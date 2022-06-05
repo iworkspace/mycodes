ede46d7a109226f4916e5eb20b0ab63c15a6c722
root <iworkspace@163.com>
2022-06-06 06:00:41 +0800

    use temporary ipv6 addr first

--- dns_v6.sh
+++ dns_v6.sh
@@ -10,13 +10,16 @@ if [ -z "$dns_name" ] || [ -z "$dns_token" ] ; then
 fi
 
 while : ; do
-	interface_ipv6=`ip a | awk  '/inet6/{print $2}'| awk -F'/' '/^2[0-9a-f:]/{print $1}'`
+	#interface_ipv6=`ip a | awk  '/inet6/{print $2}'| awk -F'/' '/^2[0-9a-f:]/{print $1}'`
+	#linux use temopray ipv6 addr first.
+	interface_ipv6=`ip a | grep 'temporary' | awk  '/inet6/{print $2}' |awk -F'/' '/^2[0-9a-f:]/{print $1}'`
 	if [  -z "$interface_ipv6" ] ; then
 		echo "no ipv6 addr ..."
 		exit 1
 	fi
 	
-	ddns_ipv6=`nslookup workspace.noip.cn | grep -A 1 "Name:" |grep "Address:"| awk '{print $2}'`
+	#ddns_ipv6=`nslookup workspace.noip.cn ? iworkspace.noip.cn | grep -A 1 "Name:" |grep "Address:"| awk '{print $2}'`
+	ddns_ipv6=`nslookup $dns_name | grep -A 1 "Name:" |grep "Address:"| awk '{print $2}'`
 	if [ "$interface_ipv6" != "$ddns_ipv6"  ] ; then
 		wget "http://www.meibu.com/ipv6zdz.asp?ipv6=$interface_ipv6&name=$dns_name&pwd=$dns_token" -o ddns_v6_push.log -O ddns_v6.result
 		sed -i 's/\r//' ddns_v6.result
